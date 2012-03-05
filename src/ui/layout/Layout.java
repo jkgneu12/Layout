3fc615ae5da387acd440ac5435b406398731e7bb
Jim George <george.j@husky.neu.edu>
2012-03-04 23:03:57 -0500

    Lists + big refactor

--- src/ui/layout/Layout.java
+++ src/ui/layout/Layout.java
@@ -3,31 +3,38 @@ import ui.activity.BaseActivity;
 import ui.layout.calc.LayoutCalc;
 import ui.wrapper.ContainerWrapper;
 import ui.wrapper.Wrapper;
-import android.graphics.Color;
+import android.view.View;
 import android.widget.FrameLayout;
 
 
 public abstract class Layout extends FrameLayout {
 	
-	private Wrapper wrapper;
+	private ContainerWrapper wrapper;
 	private float storedTranslation = -1;
 
-	public Layout(BaseActivity activity, Wrapper wrapper) {
+	public Layout(BaseActivity activity, ContainerWrapper wrapper) {
 		super(activity);
 		this.wrapper = wrapper;
 	
-		setBackgroundColor(Color.GREEN);
+		//GradientDrawable d = new GradientDrawable();
+		//d.setColor(Color.GREEN);
+		
+		//setBackgroundDrawable(d);
+		
+		//setBackgroundColor(Color.GREEN);
 		
 		setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+		
+		setOnClickListener(wrapper);
 	}
 
-	public void layoutWrappers(ContainerWrapper c, int width, int height) {
-		LayoutCalc calc = createCalc(c, width, height);
+	public void layoutWrappers() {
+		LayoutCalc calc = createCalc(wrapper);
 		calc.layoutViews();
 		calc = null;
 	}
 
-	protected abstract LayoutCalc createCalc(ContainerWrapper c, int width, int height);
+	protected abstract LayoutCalc createCalc(ContainerWrapper c);
 
 	public Wrapper getWrapper() {
 		return wrapper;
@@ -55,6 +62,10 @@ public abstract class Layout extends FrameLayout {
 
 	public void resetLayout() {
 		storedTranslation = -1;
+	}
+	
+	public View self() {
+		return this;
 	}
 	
 	
